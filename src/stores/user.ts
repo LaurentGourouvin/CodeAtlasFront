8d4575fc7aedd8f0d7aa35c21bf8c4dc1292eed2
Gourouvin Laurent <gourouvin.laurent@gmail.com>
2025-06-22 14:18:53 +0200

    feat(keycloak): update user store

--- src/stores/user.ts
+++ src/stores/user.ts
@@ -1,14 +1,18 @@
 import { defineStore } from "pinia";
 import { computed } from "vue";
 import keycloak from "@/plugins/keycloak/keycloak";
+import type { KeycloakTokenParsed } from "keycloak-js";
 
 export const useUserStore = defineStore('user', () => {
-    const userInfo = computed(() => keycloak?.tokenParsed || null)
-
-    const name = computed(() => {
+    const userInfo = computed((): KeycloakTokenParsed|null => keycloak?.tokenParsed || null)
+    console.log(userInfo.value)
+    const name = computed<string>(() => {
         return userInfo.value?.name ?? 'Utilisateur'
     })
+    const roles = computed<string[]>(() => {
+        return userInfo.value?.realm_access?.roles ?? [];
+    })
     return {
-        userInfo, name
+        userInfo, name, roles
     }
 })
